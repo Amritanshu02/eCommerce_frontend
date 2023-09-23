b36c79ce7a6fd046289b87972fd4d2dd81120267
Amritanshu02 <78687085+Amritanshu02@users.noreply.github.com>
2023-09-23 17:40:50 +0530

    added deployed backend link

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/";
+// const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "https://ecommerce-backend-nido.onrender.com";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0Y2Y2Njc4MzE2YTZkZjU1NmFmODg3YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NTEyMDYxMCwiZXhwIjoxNjk1Mzc5ODEwfQ.iDNtuonzcuad9ZRm7RMnf3XhypoDx9VFel2Oiz5pm5k";
 
 export const publicRequest = axios.create({
