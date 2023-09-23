e61714193db77ea0c8bd039e02f1f20bdc969aa6
Amritanshu02 <78687085+Amritanshu02@users.noreply.github.com>
2023-09-23 17:21:30 +0530

    Initial commit

---
+++ src/requestMethods.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8000/";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0Y2Y2Njc4MzE2YTZkZjU1NmFmODg3YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NTEyMDYxMCwiZXhwIjoxNjk1Mzc5ODEwfQ.iDNtuonzcuad9ZRm7RMnf3XhypoDx9VFel2Oiz5pm5k";
+
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    header: { token: `Bearer ${TOKEN}` }
+});
